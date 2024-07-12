8a0f94fbaab6af244d65d27104ec16129257b2a0
Vu Nguyen <74219658+vuuduu@users.noreply.github.com>
2024-07-12 00:10:32 -0400

    Added in bootstrap library

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,6 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <h1> Home Page </h1>
+        <>
+            <h1> Home Page </h1>
+            <Button href="/checkout" variant="primary"> Check Me Out </Button>
+        </>
+
     )
 }
 
